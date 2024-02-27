d0498b55216203d77057bbfdb1c3125de5cbc4c4
IGORAOLIV <igoralmeida0105@gmail.com>
2024-02-27 17:57:32 -0300

    Criando home e search

--- lib/home.dart
+++ lib/home.dart
@@ -6,28 +6,66 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.cyan.shade200,
-        title: Row(
+      backgroundColor: Colors.blue.shade100,
+      appBar: _buildAppBar(),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
           children: [
-            Icon(
-              Icons.menu,
-              color: Colors.black87,
-              size: 30,
-            ),
-            Container(
-              height: 40,
-              width: 40,
-              child: ClipRRect(
-                child: Image.asset('assets/images/avatar.jpg'),
-              ),
-            )
+            searchBox(),
           ],
         ),
       ),
-      body: Container(
-        child: Text('This is home scream.'),
-      ),
     );
   }
+}
+
+Widget searchBox() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15),
+    decoration: BoxDecoration(
+        color: Colors.white, borderRadius: BorderRadius.circular(20)),
+    child: TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(0),
+        prefixIcon: Icon(
+          Icons.search,
+          color: Colors.black87,
+          size: 20,
+        ),
+        prefixIconConstraints: BoxConstraints(
+          maxHeight: 20,
+          minWidth: 25,
+        ),
+        border: InputBorder.none,
+        hintText: 'Search',
+        hintStyle: TextStyle(color: Colors.black87),
+      ),
+    ),
+  );
+}
+
+AppBar _buildAppBar() {
+  return AppBar(
+    backgroundColor: Colors.blue.shade100,
+    elevation: 0,
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(
+          Icons.menu,
+          color: Colors.black87,
+          size: 30,
+        ),
+        Container(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset('assets/images/avatar.jpeg'),
+          ),
+        )
+      ],
+    ),
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todo_list/home.dart';
 
 void main() {
@@ -11,7 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'ToDo App',
       home: Home(),
     );
